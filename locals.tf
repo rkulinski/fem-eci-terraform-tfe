84de717224138e3430ba8590978e2381c23b23f0
robert <robert.kulinski93@gmail.com>
2024-01-05 20:24:33 +0100

    remote

--- locals.tf
+++ locals.tf
@@ -6,9 +6,10 @@ locals {
   }
   workspace = {
     "fem-eci-tfe" = {
-      description    = "Example description"
-      execution_mode = "local"
-      project_id     = module.project["fem-eci-project"].id
+      description         = "Example description"
+      execution_mode      = "remote"
+      project_id          = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "rkulinski/fem-eci-terraform-tfe"
     }
   }
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "organization_name" {
   default = "rkstudio"
   type    = string
 }
+
+variable "github_app_installation_id" {
+  default = 45800524
+  type    = number
+}
+
+variable "github_organization_name" {
+  default = "rkulinski"
+  type    = string
+}
